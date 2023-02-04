4d7d9161d8c6494415be1b8766aa94ab607446a1
anshulmokhale <89470956+anshulmokhale@users.noreply.github.com>
2023-02-04 13:25:52 +0530

    fourth day

---
+++ addEbent.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("btn");
+
+let head = document.querySelector("h1");
+
+function change() {
+    head.innerHTML = "This is changed!!!!";
+    btn.style.background = "red";
+}
+
+btn.addEventListener('click', change);
--- getCcss.js
+++
@@ -1,3 +0,0 @@
-let val = document.getElementById("ul");
-let css = getComputedStyle(val);
-console.log(css);
